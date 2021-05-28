a412893b081eae6b3e0119c11942ce2b5c1ac3b0
Dan Orban <dtorban@umn.edu>
2021-05-27 22:56:19 -0500

    Added parameters, nav, and data to PCA.

--- apps/VLExplorer/web/js/dynamics.js
+++ apps/VLExplorer/web/js/dynamics.js
@@ -47,7 +47,30 @@ function updatePCA(sample, plot) {
     if (sample.nav.keys) {
       if (!sample.keys || Object.entries(sample.keys).length != Object.entries(sample.nav.keys).length) {
         sample.keys = sample.nav.keys;
+
+        $(plot.containerId+" .pca-config").html("");
+
+        d3.select(plot.containerId+" .pca-config")
+          .selectAll("div")
+          .data(Object.entries(sample.nav.keys))
+          .enter()
+          .append("div")
+          .append("div")
+          .text((d) => {return d[0];})
+          .append("input")
+          .attr("type", "checkbox")
+          .text((d) => {
+            return d[0];
+          })
+          .property("checked", (d) => {
+              return d[1] > 0;
+          })
+          .on("change", (d,e) => {
+            sample.keys[d[0]] = d3.event.target.checked ? 1 : 0;
+            plot.reset();
+          });
       }
+
       sample.nav.keys = sample.keys;
       //console.log(plot.SVG.node());
       //plot.SVG.node().append('<div class="pca-config">'+JSON.stringify(sample.nav)+'</div>');
@@ -56,25 +79,7 @@ function updatePCA(sample, plot) {
 
       //console.log(Object.entries(sample.nav.keys));
 
-      d3.select(plot.containerId+" .pca-config")
-        .selectAll("div")
-        .data(Object.entries(sample.nav.keys))
-        .enter()
-        .append("div")
-        .append("div")
-        .text((d) => {return d[0];})
-        .append("input")
-        .attr("type", "checkbox")
-        .text((d) => {
-          return d[0];
-        })
-        .property("checked", (d) => {
-            return d[1] > 0;
-        })
-        .on("change", (d,e) => {
-          sample.keys[d[0]] = d3.event.target.checked ? 1 : 0;
-          plot.reset();
-        });
+      
         
         /*.attr("cy", function (d) { return self.zoomY(b(d)); } )
         //.attr("cy", function (d) { return y(d["Petal_Length"]); } )
--- src/VLml/VirtualLab/pca/PCAModel.cpp
+++ src/VLml/VirtualLab/pca/PCAModel.cpp
@@ -31,154 +31,6 @@ void RunPCA(arma::mat& dataset,
 
 #endif
 
-#ifdef USE_MLPACK2
-
-namespace vl {
-
-template<typename DecompositionPolicy>
-void RunPCA(arma::mat& dataset,
-            const size_t newDimension,
-            const bool scale,
-            const double varToRetain)
-{
-  PCA<DecompositionPolicy> p(scale);
-  double varRetained;
-
-    varRetained = p.Apply(dataset, newDimension);
-}
-
-class PCASample : public IModelSample {
-public:
-    PCASample(DataObject params, IModelSample* sample) : params(params), sample(sample) {
-        data["data"] = DataObject();
-        data["pca"] = DataArray();
-        data["pca2"] = DataArray();
-        da = &data["data"].get<vl::Object>();
-        d = &data["pca"].get<vl::Array>();
-        d2 = &data["pca2"].get<vl::Array>();
-
-        nav = sample->getNavigation();
-    }
-
-    virtual ~PCASample() {
-        delete sample;
-    }
-
-    virtual const DataObject& getParameters() const { return params; }
-    virtual DataObject& getNavigation() { return nav; }
-    virtual const DataObject& getData() const { return data; }
-
-    virtual void update(int id, Object& prev, Object& obj) {
-
-        double x = prev["x"].get<double>();
-        double y = prev["y"].get<double>();
-        double dx = obj["x"].get<double>() - x;
-        double dy = obj["y"].get<double>() - y;
-        double dist = std::sqrt(std::pow(dx,2.0)+std::pow(dy,2.0)) ;
-
-        arma::rowvec r;
-		r << obj["actin"].get<double>()
-			<< obj["aflow"].get<double>() 
-			<< obj["en"].get<double>() 
-			<< obj["free_actin"].get<double>() 
-			<< obj["nm"].get<double>()
-			<< std::sqrt(std::pow(obj["fx"].get<double>(),2.0)+std::pow(obj["fy"].get<double>(),2.0)) 
-            << dist
-			<< sample->getData()["rmc"].get<double>()
-			<< arma::endr;
-        if (nav["t"].get<double>() > 0.0) {
-		    rows.push_back(r);
-        }
-
-
-
-    }
-
-    virtual void update() {
-
-        std::vector<Object> prev;
-        //std::cout << JSONSerializer::instance().serialize(sample->getData()) << std::endl;
-        /*for (int i = 0; i < sample->getData()["data"].get<vl::Array>().size(); i++) {
-            prev.push_back(sample->getData()["data"].get<vl::Array>()[i].get<Object>());
-        }*/
-        prev.push_back(sample->getData().get<Object>());
-
-        sample->getNavigation() = nav;
-        sample->update();
-        
-        vl::Array array;
-        vl::Array array2;
-        if (prev.size() > 0) {
-            /*for (int i = 0; i < sample->getData()["data"].get<vl::Array>().size(); i++) {
-                Object obj = sample->getData()["data"].get<vl::Array>()[i].get<Object>();
-                update(i, prev[i], obj);
-            }*/
-            Object obj = sample->getData().get<Object>();
-            update(0, prev[0], obj);
-
-            if (rows.size() > 2) {
-                arma::mat A(rows.size(), rows[0].n_cols);
-            
-                //B << sample->getNavigation()["t"].get<double>() << arma::endr << sample->getNavigation()["t"].get<double>() << arma::endr;
-                for (int i = 0; i < rows.size(); i++) {
-                    A.row(i) = rows[i];
-                }
-                A = A.t();
-                RunPCA<ExactSVDPolicy>(A, 2, true, 1.0);
-                A = A.t();
-
-
-                for (int f = 0; f < rows.size(); f+=10*prev.size()) {
-                    for (int i = 0; i < prev.size(); i++) {
-                        vl::DataObject obj;
-                        //std::cout << A.row(f);
-                        obj["x"] = DoubleDataValue(A(f+i,0));
-                        obj["y"] = DoubleDataValue(A(f+i,1));
-                        obj["id"] = DoubleDataValue(i);
-                        obj["t"] = DoubleDataValue(f/prev.size());
-                        array2.push_back(obj);
-                    }
-                }
-
-                for (int f = rows.size() - 20*prev.size(); f < rows.size(); f+=1*prev.size()) {
-                    for (int i = 0; i < prev.size(); i++) {
-                        vl::DataObject obj;
-                        //std::cout << A.row(f);
-                        obj["x"] = DoubleDataValue(A(f+i,0));
-                        obj["y"] = DoubleDataValue(A(f+i,1));
-                        obj["id"] = DoubleDataValue(i);
-                        obj["t"] = DoubleDataValue(f/prev.size());
-                        array.push_back(obj);
-                    }
-                }
-            }
-        }
-        *d = array;
-        *d2 = array2;
-        *da = sample->getData().get<vl::Object>();
-    }
-
-private:
-    IModelSample* sample;
-    DataObject params;
-    DataObject nav;
-    DataObject data;
-    vl::Array* d;
-    vl::Array* d2;
-    vl::Object* da;
-	std::vector<arma::rowvec> rows;
-};
-
-IModelSample* PCAModel::create(const DataObject& params) {
-    return new PCASample(params, model->create(params));
-}
-
-}
-
-#else
-
-#endif
-
 namespace vl {
 
 class PCAModelSample : public IModelSample, public IDataConsumer {
@@ -195,7 +47,6 @@ public:
 	void update(IUpdateCallback* callback);
 	virtual void consume(IModel& model, IModelSample& sample);
 
-
 private:
     DataObject params;
     DataObject nav;
@@ -205,11 +56,13 @@ private:
     int kmeans_calc;
     std::vector<std::string> columns;
     std::vector<DataObject> dataRows;
+    std::vector<DataObject> navRows;
+    std::vector<IModelSample*> samplePtr;
+    std::map<IModelSample*, DataObject> paramRows;
     DataObject keys;
+    std::map<std::string, int> keyType;
     int prevColumnSize;
 #ifdef USE_MLPACK
-	//std::vector<arma::rowvec> rows;
-    //arma::mat prevCentroids;
     arma::Row<size_t> assignments;
     arma::mat centroids;
 #endif
@@ -227,11 +80,20 @@ void PCAModelSample::update() {
         //std::cout << "update PCA" << std::endl;
 
         nav["keys"] = keys;
+        int params = 0;
+        int other = 0;
 
         std::vector<std::string> cols;
         for (DataObject::const_iterator it = keys.begin(); it != keys.end(); it++) {
             if (it->second.get<double>() > 0.0001) {
                 cols.push_back(it->first);
+                int type = keyType[it->first];
+                if (type == 0) {
+                    params++;
+                }
+                else {
+                    other++;
+                }
             }
         }
         //std::cout << numCols << std::endl;
@@ -244,13 +106,44 @@ void PCAModelSample::update() {
         pca->clear();
 
 #ifdef USE_MLPACK
-        if (cols.size() > 1 && dataRows.size() > 2) {
-                arma::mat A(dataRows.size(), cols.size());
-            
-                //B << sample->getNavigation()["t"].get<double>() << arma::endr << sample->getNavigation()["t"].get<double>() << arma::endr;
-                for (int i = 0; i < dataRows.size(); i++) {
-                    for (int f = 0; f < cols.size(); f++) {
-                        A(i,f) = dataRows[i][cols[f]].get<double>();
+        if (cols.size() > 1 && (dataRows.size() > 2 || samplePtr.size() > 2)) {
+                int numRows = dataRows.size();
+                if (params > 0 && other == 0) {
+                    numRows = paramRows.size();
+                }
+
+                arma::mat A(numRows, cols.size());
+                
+                if (params > 0 && other ==0) {
+                    int i = 0;
+                    for (std::map<IModelSample*, DataObject>::iterator it = paramRows.begin(); it != paramRows.end(); it++) {
+                        for (int f = 0; f < cols.size(); f++) {
+                            A(i,f) = it->second[cols[f]].get<double>();
+                        }
+                        i++;
+                    }
+                }
+                else {
+                    //B << sample->getNavigation()["t"].get<double>() << arma::endr << sample->getNavigation()["t"].get<double>() << arma::endr;
+                    for (int i = 0; i < dataRows.size(); i++) {
+                        for (int f = 0; f < cols.size(); f++) {
+                            double val;
+                            const std::string& key = cols[f];
+                            int type = keyType[key];
+                            switch (type)
+                            {
+                            case 0:
+                                val = paramRows[samplePtr[i]][key].get<double>();
+                                break;
+                            case 1:
+                                val = navRows[i][key].get<double>();
+                                break;
+                            case 2:
+                                val = dataRows[i][key].get<double>();
+                                break;
+                            }
+                            A(i,f) = val;
+                        }
                     }
                 }
 
@@ -262,7 +155,6 @@ void PCAModelSample::update() {
 
                 int clusterNum = 10;
 
-                //std::cout << kmeans_calc << " " << rows.size()/100 << std::endl;
                 int blah = dataRows.size()/100;
                 if (kmeans_calc < blah) {
                     //std::cout << "Calc KMeans" << std::endl;
@@ -281,67 +173,25 @@ void PCAModelSample::update() {
                 }
                 
 
-                /*std::vector<int> oldIndices;
-                if (prevCentroids.size() == centroids.size()) {
-                    // Figure out reordering
-                    //std::cout << prevCentroids << " " << prevCentroids.size() << std::endl;
-                    //std::cout << centroids << " " << centroids.size() << std::endl;
-                    
-
-                    for (int i = 0; i < 5; i++)  {
-                        double min;
-                        int oldIndex = 0;
-                        for (int f = 0; f < 5; f++)  {
-                            double val = std::pow(prevCentroids(0,i)-centroids(0,f),2) + std::pow(prevCentroids(1,i)-centroids(1,f),2);
-                            if (f == 0 || val < min) {
-                                min = val;
-                                oldIndex = f;
-                            }
-                        }
-                        oldIndices.push_back(oldIndex);
-                    }
-                }
-                else {
-                    for (int i = 0; i < 5; i++)  {
-                        oldIndices.push_back(i);
-                    }
-                }
-                prevCentroids = centroids;*/
-
-                //A = A.t();
-
-                //pca->clear();
-
-                for (int f = 0; f < dataRows.size(); f++) {
+                for (int f = 0; f < numRows; f++) {
                         vl::DataObject obj;
-                        //std::cout << A.row(f);
                         obj["x"] = DoubleDataValue(A(0,f));
                         obj["y"] = DoubleDataValue(A(1,f));
-                        //obj["id"] = DoubleDataValue(i);
-                        //obj["cluster"] = DoubleDataValue(oldIndices[assignments[f]]+1);
                         if (assignments.size() <= f) {
                             obj["cluster"] = DoubleDataValue(1);
                         }
                         else {
                             obj["cluster"] = DoubleDataValue(1);//assignments[f]+1);
-                            //obj["cluster"] = DoubleDataValue(oldIndices[assignments[f]]+1);
                         }
-                        //obj["cluster"] = DoubleDataValue(1);
-                        //obj["t"] = DoubleDataValue(f/prev.size());
                         pca->push_back(obj);
                 }
 
-                if (centroids.size() == clusterNum*2) {
-                    //std::cout << centroids << std::endl;
-                    
+                if (centroids.size() == clusterNum*2) {                   
                     for (int f = 0; f < clusterNum; f++) {
                             vl::DataObject obj;
-                            //std::cout << A.row(f);
                             obj["x"] = DoubleDataValue(centroids(0,f));
                             obj["y"] = DoubleDataValue(centroids(1,f));
-                            //obj["id"] = DoubleDataValue(i);
                             obj["cluster"] = DoubleDataValue(0);
-                            //obj["t"] = DoubleDataValue(f/prev.size());
                             pca->push_back(obj);
                     }
                 }
@@ -359,30 +209,44 @@ void PCAModelSample::update() {
 void PCAModelSample::update(IUpdateCallback* callback) {
     keys = this->nav["keys"];
     this->callback = callback;
-    //ModelSampleDecorator::update(callback);
-    //producer->produce(*model, *sample);
 }
 
 void PCAModelSample::consume(IModel& model, IModelSample& sample) {
-    //return 0;
-	//std::cout << sample.getData()["x"].get<double>() << " " << sample.getData()["y"].get<double>() << std::endl;
-	//std::cout << model.getName() << " " << &sample << std::endl;
 
     const DataObject& obj = sample.getData();
     const DataObject& nav = sample.getNavigation();
+    const DataObject& params = sample.getParameters();
 
+    navRows.push_back(nav);
     dataRows.push_back(obj);
+    samplePtr.push_back(&sample);
+    if (paramRows.find(&sample) == paramRows.end()) {
+        paramRows[&sample] = sample.getParameters();
+    }
+
+    for (DataObject::const_iterator it = params.begin(); it != params.end(); it++) {
+        if (keys.find(it->first) == keys.end()) {
+            std::cout << it->first << std::endl;
+            keys[it->first] = DoubleDataValue(0);
+            keyType[it->first] = 0;
+        }
+    }
+    for (DataObject::const_iterator it = nav.begin(); it != nav.end(); it++) {
+        if (keys.find(it->first) == keys.end()) {
+            std::cout << it->first << std::endl;
+            keys[it->first] = DoubleDataValue(0);
+            keyType[it->first] = 1;
+        }
+    }
     for (DataObject::const_iterator it = obj.begin(); it != obj.end(); it++) {
         if (keys.find(it->first) == keys.end()) {
             std::cout << it->first << std::endl;
             keys[it->first] = DoubleDataValue(0);
+            keyType[it->first] = 2;
         }
     }
 
     this->nav["keys"] = keys;
-
-#ifdef USE_MLPACK
-    //std::cout << " time: " << nav["t"].get<double>() << " " << rows.size() << std::endl;
 
     const Object& dataSetObj = obj.get<Object>();
 
@@ -394,31 +258,6 @@ void PCAModelSample::consume(IModel& model, IModelSample& sample) {
         }
     }
 
-    /*if (columns.size() > 0) {
-        arma::rowvec r(columns.size());
-        if (nav["t"].get<double>() > 0.0) {
-            for (int i = 0; i < columns.size(); i++) {
-                r(i) = obj[columns[i]].get<double>();
-            }
-            rows.push_back(r);
-        }
-    }*/
-    
-
-    /*arma::rowvec r;
-    if (nav["t"].get<double>() > 0.0) {
-        r << obj["actin"].get<double>()
-        << obj["aflow"].get<double>() 
-        << obj["en"].get<double>() 
-        << obj["free_actin"].get<double>() 
-        << obj["nm"].get<double>()
-        << std::sqrt(std::pow(obj["fx"].get<double>(),2.0)+std::pow(obj["fy"].get<double>(),2.0)) 
-        //<< dist
-        << obj["rmc"].get<double>()
-        << arma::endr;
-        rows.push_back(r);
-    }*/
-#endif
 
 	update();
 }
