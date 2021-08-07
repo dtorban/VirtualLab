5518c26ad9deb6a2441c1c063e33cf2e8ab0cf5d
Dan Orban <dtorban@umn.edu>
2021-08-07 12:20:10 -0500

    Changes to support interactive model.

--- bin/msi-cluster.sh
+++ bin/msi-cluster.sh
@@ -3,5 +3,5 @@
 ROOTDIR=`git rev-parse --show-toplevel`
 #${ROOTDIR}/bin/build-msi.sh
 
-ssh -R 3457:172.21.0.2:3457 -L 172.21.0.1:12342:127.0.0.1:12342 dtorban@ln1001.msi.umn.edu ssh -R 172.21.0.2:3457:127.0.0.1:3457 -L 127.0.0.1:12342:127.0.0.1:12342 cn0073 '/home/keefedf/dtorban/project/VirtualLab/bin/msi-setup-worker.sh 12342' &
-ssh -R 3457:172.21.0.2:3457 -L 172.21.0.1:12343:127.0.0.1:12343 dtorban@ln1001.msi.umn.edu ssh -R 172.21.0.2:3457:127.0.0.1:3457 -L 127.0.0.1:12343:127.0.0.1:12343 cn0074 '/home/keefedf/dtorban/project/VirtualLab/bin/msi-setup-worker.sh 12343' &
+ssh -R 3457:172.21.0.2:3457 -L 172.21.0.1:12342:127.0.0.1:12342 dtorban@ln1002.msi.umn.edu ssh -R 172.21.0.2:3457:127.0.0.1:3457 -L 127.0.0.1:12342:127.0.0.1:12342 cn1159 '/home/keefedf/dtorban/project/VirtualLab/bin/msi-setup-worker.sh 12342' &
+#ssh -R 3457:172.21.0.2:3457 -L 172.21.0.1:12343:127.0.0.1:12343 dtorban@ln1001.msi.umn.edu ssh -R 172.21.0.2:3457:127.0.0.1:3457 -L 127.0.0.1:12343:127.0.0.1:12343 cn0074 '/home/keefedf/dtorban/project/VirtualLab/bin/msi-setup-worker.sh 12343' &
--- bin/msi-kill-cluster.sh
+++ bin/msi-kill-cluster.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ps aux | grep msi-setup-worker.sh | awk '{print $2}' | xargs -r kill -9
-ssh dtorban@ln1001.msi.umn.edu "ps aux | grep msi-setup-worker.sh | awk '{print \$2}' | xargs -r kill -9" &
-ssh dtorban@ln1001.msi.umn.edu ssh cn0073 "ps aux | grep CellModel | awk '{print \$2}' | xargs -r kill -9" &
-ssh dtorban@ln1001.msi.umn.edu ssh cn0074 "ps aux | grep CellModel | awk '{print \$2}' | xargs -r kill -9" &
--
+ssh dtorban@ln1002.msi.umn.edu "ps aux | grep msi-setup-worker.sh | awk '{print \$2}' | xargs -r kill -9" &
+#ssh dtorban@ln1002.msi.umn.edu ssh cn1068 "ps aux | grep CellModel | awk '{print \$2}' | xargs -r kill -9" &
+ssh dtorban@ln1002.msi.umn.edu ssh cn1159 "/home/keefedf/dtorban/project/VirtualLab/bin/msi-kill-worker.sh" &
+#ssh dtorban@ln1001.msi.umn.edu ssh cn0074 "ps aux | grep CellModel | awk '{print \$2}' | xargs -r kill -9" &
